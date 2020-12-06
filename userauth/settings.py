28738439c6d5b65a82ebd0e37487a3eb0385c2bf
maxwell neideigh <neideigh0419@gmail.com>
2020-12-06 12:07:32 -0800

    new

--- userauth/settings.py
+++ userauth/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions.backends.db',
+    'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'userauthwebsite',
